64cc9b06965cbfc726af3d07404422fae7df9889
Hrlpe <hooman.larijani@gmail.com>
2024-09-06 18:38:06 +0330

    updated db pth

--- resume_app/settings.py
+++ resume_app/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'resume_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / '/home/hrlpe/resume_app/db.sqlite3',
     }
 }
 
