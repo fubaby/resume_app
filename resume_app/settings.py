42852431019450f392188c8126c9490b8fbf77cb
Homan Larijani <hooman.larijani@gmail.com>
2024-09-07 20:20:08 +0430

    changes from wsl

--- resume_app/settings.py
+++ resume_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-v8bzoj5)*&_%x-yy7o*z-2$*m1uuo*hbtb(n)%@bboej@%wkox
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['hrlpe.pythonanywhere.com']
+ALLOWED_HOSTS = ['hrlpe.pythonanywhere.com','127.0.0.1','localhost']
 
 
 # Application definition
